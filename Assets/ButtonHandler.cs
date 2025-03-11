fe49bbc957abecfe89da7f4e07fe61dd86037d61
aylax <aylax123@gmail.com>
2025-03-11 19:03:00 -0400

    Button !!!

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void ButtonClickFunction()
+    {
+        Debug.Log("Button clicked!");
+        // Add other code for the button's action here
+    }
+}
